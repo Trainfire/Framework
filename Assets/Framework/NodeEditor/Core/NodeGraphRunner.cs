f7aeb91ebfcffb05758d27c8a086f67e5112f7b9
Trainfire <aarongarcha@gmail.com>
2017-11-12 01:03:10 +0100

    Temp code for steppable graph execution.

--- Assets/Framework/NodeEditor/Core/NodeGraphRunner.cs
+++ Assets/Framework/NodeEditor/Core/NodeGraphRunner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine.Assertions;
 
 namespace Framework.NodeSystem
 {
@@ -10,6 +11,7 @@ namespace Framework.NodeSystem
         private const int MaxExecutions = 256;
 
         private NodeGraph _graph;
+        private NodeExecution _execution;
         private int _executions;
 
         public NodeGraphRunner() { }
@@ -59,7 +61,10 @@ namespace Framework.NodeSystem
         {
             DebugEx.Log<NodeGraphRunner>("Move next: {0} ({1})", nodeExecute.Name, nodeExecute.ID);
 
-            Prepare(nodeExecute);
+            if (_execution == null)
+                _execution = new NodeExecution(_graph.Helper, true);
+
+            _execution.Start(nodeExecute);
 
             nodeExecute.Execute(new NodeExecuteParameters());
 
@@ -81,6 +86,106 @@ namespace Framework.NodeSystem
                 {
                     DebugEx.Log<NodeGraphRunner>("Finished execution of all nodes.");
                 }
+            }
+        }
+    }
+
+    public class NodeExecution
+    {
+        public bool Completed { get; private set; }
+
+        private NodeGraphHelper _graphHelper;
+        private bool _autoIterate;
+
+        private Stack<NodeExecutionGroup> _stack;
+
+        public NodeExecution(NodeGraphHelper graphHelper, bool autoIterate = false)
+        {
+            _graphHelper = graphHelper;
+            _autoIterate = autoIterate;
+        }
+
+        public void Start(Node startNode)
+        {
+            _stack = new Stack<NodeExecutionGroup>();
+
+            // Auto-iterate on start node.
+            _stack.Push(new NodeExecutionGroup(startNode, _graphHelper));
+
+            if (_autoIterate)
+                Iterate();
+        }
+
+        public void Iterate()
+        {
+            if (_stack.Count == 0)
+            {
+                Completed = true;
+                return;
+            }
+
+            var currentGroup = _stack.Peek();
+            currentGroup.Iterate();
+
+            if (currentGroup.SubGroup != null)
+            {
+                _stack.Push(currentGroup.SubGroup);
+            }
+            else if (currentGroup.Finished)
+            {
+                // Node is prepared. Calculate it.
+                currentGroup.Node.Calculate();
+
+                // Pop from stack.
+                _stack.Pop();
+            }
+
+            if (_autoIterate)
+                Iterate();
+        }
+    }
+
+    public class NodeExecutionGroup
+    {
+        public Node Node { get; private set; }
+        public NodeExecutionGroup SubGroup { get; private set; }
+        public bool Finished { get; private set; }
+
+        private NodeGraphHelper _graphHelper;
+        private int _currentPin;
+        private int _pinCount;
+
+        public NodeExecutionGroup(Node node, NodeGraphHelper graphHelper)
+        {
+            Node = node;
+
+            _graphHelper = graphHelper;
+            _pinCount = node.InputPins.Count;
+        }
+
+        public void Iterate()
+        {
+            // NB: Nodes such as constants have no input pins so no sub-groups will ever be created for them.
+            if (_pinCount != 0)
+            {
+                var pin = Node.InputPins[_currentPin];
+                var connection = _graphHelper.GetConnectionFromStartPin(pin);
+                if (connection != null)
+                {
+                    SubGroup = new NodeExecutionGroup(connection.EndNode, _graphHelper);
+                }
+                else
+                {
+                    SubGroup = null;
+                }
+
+                _currentPin++;
+            }
+
+            if (_currentPin == _pinCount || _pinCount == 0)
+            {
+                SubGroup = null;
+                Finished = true;
             }
         }
     }
