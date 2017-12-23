33d876b3014569af1a3d4aa6ff4762b93e26f09e
Trainfire <aarongarcha@gmail.com>
2017-12-23 12:32:48 +0000

    Add parsing from string for graph variables.

--- Assets/Framework/NodeEditor/Core/System/NodeGraphVariable.cs
+++ Assets/Framework/NodeEditor/Core/System/NodeGraphVariable.cs
@@ -9,11 +9,11 @@ namespace NodeSystem
         Set,
     }
 
-    public class NodeGraphVariable
+    public abstract class NodeGraphVariable
     {
         public string Name { get; private set; }
         public string ID { get; private set; }
-        public string Type { get; private set; }
+        public string Type { get; protected set; }
 
         public Type ActualType { get { return System.Type.GetType(Type); } }
 
@@ -23,6 +23,43 @@ namespace NodeSystem
             ID = data.ID;
             Type = data.VariableType;
         }
+
+        public void Parse(string value)
+        {
+            SetIfType(typeof(float), () =>
+            {
+                float outValue;
+                float.TryParse(value, out outValue);
+                Set(outValue);
+            });
+
+            SetIfType(typeof(int), () =>
+            {
+                int outValue;
+                int.TryParse(value, out outValue);
+                Set(outValue);
+            });
+
+            SetIfType(typeof(bool), () =>
+            {
+                bool outValue;
+                bool.TryParse(value, out outValue);
+                Set(outValue);
+            });
+
+            SetIfType(typeof(string), () => Set(value));
+        }
+
+        void SetIfType(Type type, Action onIsType)
+        {
+            if (ActualType == type)
+                onIsType();
+        }
+
+        protected virtual void Set(float value) { (this as NodeGraphVariable<float>).Value = value; }
+        protected virtual void Set(int value) { (this as NodeGraphVariable<int>).Value = value; }
+        protected virtual void Set(bool value) { (this as NodeGraphVariable<bool>).Value = value; }
+        protected virtual void Set(string value) { (this as NodeGraphVariable<string>).Value = value; }
     }
 
     public class NodeGraphVariable<T> : NodeGraphVariable
